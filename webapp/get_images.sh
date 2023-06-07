e4fd6a7e43235e845177624c7d8585d93f3951a1
6rawn <103933406+6rawn@users.noreply.github.com>
2023-06-07 23:28:41 +0900

    Use `docker compose exec`

--- webapp/get_images.sh
+++ webapp/get_images.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-mysql_container=$(docker ps | grep $(docker compose ps | grep mysql | cut -f 1 -d ' ') | cut -f 1 -d ' ')
-docker exec -it $mysql_container mysql -u root -proot -D isuconp -e "SELECT id, mime from posts;" > _get_image.sh
+docker compose exec -it mysql mysql -u root -proot -D isuconp -e "SELECT id, mime from posts;" > _get_image.sh
 
 # -e 's#\(\.[a-zA-z]*\)#\1 \&#g' -e 's#\n##g'
 gsed -i'' -e '$d' -e '1,4d' -e 's#| *##g' -e 's# image/#\.#g' -e 's#jpeg#jpg#g' -e 's#^#http://localhost/image/#g' -e 's#^#curl -sO #g'  _get_image.sh
